d43823ebbbfe6e84953933fb9e225f2610861929
dsiri12 <dsiri12@gmail.com>
2024-06-27 22:20:37 -0400

    html and javascript changes made

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function addTodo(){
     let text=document.getElementById('addTodo').value;
     console.log(text);
  }
+
+//  create new function
+
+//  create variable that saves whatever value that is inside of this function
+
+//  log this function
+
+function deleteTodo(){
+    let text=document.getElementById('deleteTodo').value;
+    console.log(text);
+}
