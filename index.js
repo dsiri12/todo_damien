4d49d0af19555dd6c34ca427d1c67da3765e23d4
dsiri12 <dsiri12@gmail.com>
2024-07-01 23:18:19 -0400

    changes made mostly within javascript

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 console.log("Hello World");
 
+
+
 function addTodo(){
     let text=document.getElementById('addTodo').value;
-    console.log(text);
+    let todoitem = document.createElement('li');
+    let todoItemText = document.createElement('p')
+    let todoitems=document.getElementById('todoitems');
+    let checkTodo = document.createElement('input')
+
+    todoItemText.textContent = text
+    todoitem.classList.add('todoitem')
+    checkTodo.setAttribute('type', 'checkbox')
+    
+
+    todoitem.appendChild(todoItemText)
+    todoitem.appendChild(checkTodo)
+    todoitems.appendChild(todoitem)
+    document.getElementById("addTodo").value = "";
 }
 
 //  create new function
