791d3bfe3dbebb9473a81cfad3252ebc7743b85e
dsiri12 <dsiri12@gmail.com>
2024-07-03 20:33:55 -0400

    adding save todo in javascript file

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 console.log("Hello World");
+const todoList = []
+const saveTodo = window.localStorage.getItem('todoitem')
+todoList.join(JSON.parse(saveTodo))
+console.log(saveTodo)
+let todoitems=document.getElementById('todoitems');
 
-
+for (let i = 0; i < JSON.parse(saveTodo).length; i++) {
+    let saveTodoItem = document.createElement('li');
+    let saveTodoItemText = document.createElement('p')
+    let saveCheckTodo = document.createElement('input')
+    saveTodoItemText.textContent = JSON.parse(saveTodo)[i]
+    saveTodoItem.classList.add('todoitem')
+    saveCheckTodo.setAttribute('type', 'checkbox')
+    saveTodoItem.appendChild(saveTodoItemText)
+    saveTodoItem.appendChild(saveCheckTodo)
+    console.log(todoitems)
+    todoitems.appendChild(saveTodoItem)
+}
 
 function addTodo(){
     let text=document.getElementById('addTodo').value;
+    todoList.push(text)
+    window.localStorage.setItem('todoitem', JSON.stringify(todoList))
+    
+    console.log(todoList)
+
     let todoitem = document.createElement('li');
     let todoItemText = document.createElement('p')
-    let todoitems=document.getElementById('todoitems');
+    
     let checkTodo = document.createElement('input')
 
     todoItemText.textContent = text
