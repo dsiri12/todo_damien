7b2e2654380fb542f0ab313e1c7766d4bbc41bf3
dsiri12 <dsiri12@gmail.com>
2024-07-04 20:22:18 -0400

    practice of javascript code

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ for (let i = 0; i < JSON.parse(saveTodo).length; i++) {
     let saveTodoItem = document.createElement('li');
     let saveTodoItemText = document.createElement('p')
     let saveCheckTodo = document.createElement('input')
+
     saveTodoItemText.textContent = JSON.parse(saveTodo)[i]
     saveTodoItem.classList.add('todoitem')
     saveCheckTodo.setAttribute('type', 'checkbox')
@@ -51,3 +52,52 @@ function deleteTodo(){
     let text=document.getElementById('deleteTodo').value;
     console.log(text);
 }
+
+// lines of code that possibly connect with the addTodo function and the for loop function
+
+// console.log("Hello World");
+// const todoList = []
+// const saveTodo = window.localStorage.getItem('todoitem)
+// todoList.join(JSON.parse(saveTodo))
+// console.log(saveTodo)
+// let todoitems=document.getElementById('todoitems);
+
+// the addTodo function that was done this past Monday
+
+// function addTodo(){
+//      let text=document.getElementById('addTodo').value;
+//      todoList.push(text)
+//      window.localStorage.setItem('todoitem', JSON.stringify(todoList))
+
+//      console.log(todoList)
+
+//      let todoitem = document.createElement('li');
+//      let todoItemText = document.createElement('p')
+
+//      let checkTodo = document.createElement('p');
+
+//      todoItemText.textContent = text
+//      todoitem.classList.add('todoitem')
+//      checkTodo.setAttribute('type', 'checkbox')
+
+//      todoitem.appendChild(todoItemText)
+//      todoitem.appendChild(checkTodo)
+//      todoitems.appendChild(todoitem)
+//      document.getElementById("addTodo").value = "";
+// }
+
+// the for loop function that was done yesterday being a Wednesday which was this past Wednesday
+
+// for () {
+//      let saveTodoItem = document.createElement('li');
+//      let saveTodoItemText = document.createElement('p')
+//      let saveCheckTodo = document.createElement('input')
+
+//      saveTodoItemText.textContent = JSON.parse(saveTodo)[i]
+//      saveTodoItem.classList.add('todoitem')
+//      saveCheckTodo.setAttribute('type', 'checkbox')
+//      saveTodoItem.appendChild(saveTodoItemText)
+//      saveTodoItem.appendChild(saveCheckTodo)
+//      console.log(todoitems)
+//      todoitems.appendChild(saveTodoItem)
+// }
